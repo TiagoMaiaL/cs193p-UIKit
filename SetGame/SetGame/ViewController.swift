a17ef5ce955ac5b2c8ef43acc7e1c111629ed97c
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-01-26 09:19:03 -0200

    Adding core components to UI.

--- SetGame/SetGame/ViewController.swift
+++ SetGame/SetGame/ViewController.swift
@@ -13,13 +13,28 @@ class ViewController: UIViewController {
   /// The main set game.
   private var setGame = SetGame()
   
+  /// The card buttons being displayed in the UI.
+  @IBOutlet var cardButtons: [UIButton]!
+  
   // MARK: Life cycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     _ = setGame.dealCards(forAmount: 12)
   }
 
+  // MARK: Actions
+  
+  @IBAction func didTapCard(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func didTapDealMore(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func didTapNewGame(_ sender: UIButton) {
+    
+  }
 }
 
