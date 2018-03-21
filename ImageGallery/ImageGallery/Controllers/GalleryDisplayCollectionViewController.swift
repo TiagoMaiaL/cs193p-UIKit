3b44b7809b98a59567ea4974ea9c1584d4f242f8
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-03-21 18:42:05 -0300

    AnimatedSetGame: Adding the image cell class.

--- ImageGallery/ImageGallery/Controllers/GalleryDisplayCollectionViewController.swift
+++ ImageGallery/ImageGallery/Controllers/GalleryDisplayCollectionViewController.swift
@@ -27,41 +27,23 @@ class GalleryDisplayCollectionViewController: UICollectionViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
     // Register cell classes
+    // TODO: Check if this is going to be necessary.
     self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-    
-    // Do any additional setup after loading the view.
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  /*
-   // MARK: - Navigation
+  // MARK: - Navigation
    
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using [segue destinationViewController].
-   // Pass the selected object to the new view controller.
-   }
-   */
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+  }
+
   
   // MARK: UICollectionViewDataSource
   
-  override func numberOfSections(in collectionView: UICollectionView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
-  }
-  
-  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of items
-    return 0
+    return gallery.images.count
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
---
+++ ImageGallery/ImageGallery/Views/ImageCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Tiago Maia Lopes on 21/03/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+/// The cell in charge of displaying a single image of the gallery.
+class ImageCollectionViewCell: UICollectionViewCell {
+
+  /// The cell's image view.
+  @IBOutlet weak var imageView: UIImageView!
+  
+  /// The cell's loading spinner.
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+  /// The cell's loading flag.
+  var isLoading = false {
+    didSet {
+      activityIndicator.isHidden = !isLoading
+    }
+  }
+  
+}
