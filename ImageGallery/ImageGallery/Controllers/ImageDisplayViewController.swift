fc92b083df5d48c7dbcc1a4b59f9fd60eda69475
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-03-21 14:49:31 -0300

    ImageGallery: Adding the controller files. Adding the model files.

---
+++ ImageGallery/ImageGallery/Controllers/ImageDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageDisplayViewController.swift
+//  ImageGallery
+//
+//  Created by Tiago Maia Lopes on 21/03/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ImageDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ImageGallery/ImageGallery/Models/ImageGallery.swift
@@ -0,0 +1,31 @@
+//
+//  ImageGallery.swift
+//  ImageGallery
+//
+//  Created by Tiago Maia Lopes on 21/03/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
+
+/// Model representing a gallery with it's images.
+struct ImageGallery {
+  
+  /// Model representing a gallery's image.
+  struct Image {
+    
+    // MARK: Properties
+    
+    /// The image's URL.
+    let imagePath: URL
+    
+    /// The image's aspect ratio.
+    let aspectRatio: Double
+  }
+  
+  // MARK: Properties
+  
+  /// The gallery's images.
+  var images: [Image]
+  
+}
