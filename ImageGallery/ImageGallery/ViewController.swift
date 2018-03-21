62c814798119f267ec5904a1ebc2711ca5730ebf
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-03-21 14:26:58 -0300

    ImageGallery: Initial commit.

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Tiago Maia Lopes on 21/03/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
