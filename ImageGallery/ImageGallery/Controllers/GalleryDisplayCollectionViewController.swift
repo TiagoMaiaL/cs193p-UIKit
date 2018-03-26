f1030f7df619ac1da7759604a752b0017cdd5fb0
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-03-26 12:08:20 -0300

    ImageGallery: Adding local drag and drop to collection view.

--- ImageGallery/ImageGallery/Controllers/GalleryDisplayCollectionViewController.swift
+++ ImageGallery/ImageGallery/Controllers/GalleryDisplayCollectionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private let reuseIdentifier = "imageCell"
 
-class GalleryDisplayCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDropDelegate {
+class GalleryDisplayCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDropDelegate, UICollectionViewDragDelegate {
 
   // MARK: - Properties
   
@@ -34,7 +34,8 @@ class GalleryDisplayCollectionViewController: UICollectionViewController, UIColl
   
   override func loadView() {
     super.loadView()
-    collectionView?.dropDelegate = self
+    collectionView!.dragDelegate = self
+    collectionView!.dropDelegate = self
   }
   
   // MARK: - Navigation
@@ -99,75 +100,126 @@ class GalleryDisplayCollectionViewController: UICollectionViewController, UIColl
     return CGSize(width: itemWidth, height: itemHeight)
   }
   
+  // MARK: - Collection view drag delegate
+  
+  func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+    session.localContext = collectionView
+    return getDragItems(at: indexPath)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+    return getDragItems(at: indexPath)
+  }
+  
+  private func getDragItems(at indexPath: IndexPath) -> [UIDragItem] {
+    var dragItems = [UIDragItem]()
+    
+    if let galleryImage = getImage(at: indexPath) {
+//      if let imageData = galleryImage.imageData, let image = UIImage(data: imageData) {
+//        let imageItem = UIDragItem(itemProvider: NSItemProvider(object: image))
+//        imageItem.localObject = galleryImage
+//        dragItems.append(imageItem)
+//      }
+      
+      if let imageURL = galleryImage.imagePath as NSURL? {
+        let urlItem = UIDragItem(itemProvider: NSItemProvider(object: imageURL))
+        urlItem.localObject = galleryImage
+        dragItems.append(urlItem)
+      }
+    }
+    
+    return dragItems
+  }
+  
   // MARK: - Collection view drop delegate
   
   func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
-    return session.canLoadObjects(ofClass: URL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    if collectionView.hasActiveDrag {
+      // if the drag is from this collection view, the image isn't needed.
+      print("=D")
+      return session.canLoadObjects(ofClass: URL.self)
+    } else {
+      return session.canLoadObjects(ofClass: URL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    }
   }
   
   func collectionView(_ collectionView: UICollectionView,
                       dropSessionDidUpdate session: UIDropSession,
                       withDestinationIndexPath destinationIndexPath: IndexPath?
     ) -> UICollectionViewDropProposal {
-    return UICollectionViewDropProposal(
-      operation: .copy,
-      intent: .insertAtDestinationIndexPath
-    )
+    // Determines if the drag was initiated from this app, in case of reordering.
+    let isDragFromThisApp = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+    
+    return UICollectionViewDropProposal(operation: isDragFromThisApp ? .move : .copy, intent: .insertAtDestinationIndexPath)
   }
   
-  func collectionView(_ collectionView: UICollectionView,
-                      performDropWith coordinator: UICollectionViewDropCoordinator) {
-    // For now it's only going to accept drops from outside the app.
-    if let item = coordinator.items.last {
-      let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
-      
-      let placeholderContext = coordinator.drop(
-        item.dragItem,
-        to: UICollectionViewDropPlaceholder(
-          insertionIndexPath: destinationIndexPath,
-          reuseIdentifier: reuseIdentifier
+  func collectionView(
+    _ collectionView: UICollectionView,
+    performDropWith coordinator: UICollectionViewDropCoordinator
+  ) {
+    let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+    
+    for item in coordinator.items {
+      if let sourceIndexPath = item.sourceIndexPath {
+        // The drag was initiated from this collection view.
+        if let galleryImage = item.dragItem.localObject as? ImageGallery.Image {
+          collectionView.performBatchUpdates({
+            self.gallery.images.remove(at: sourceIndexPath.item)
+            self.gallery.images.insert(galleryImage, at: destinationIndexPath.item)
+            collectionView.deleteItems(at: [sourceIndexPath])
+            collectionView.insertItems(at: [destinationIndexPath])
+          })
+          coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+        }
+        
+      } else {
+        // The drag was initiated from outside of the app.
+        let placeholderContext = coordinator.drop(
+          item.dragItem,
+          to: UICollectionViewDropPlaceholder(
+            insertionIndexPath: destinationIndexPath,
+            reuseIdentifier: reuseIdentifier
+          )
         )
-      )
-      
-      // Creates a new image to hold the place for the dragged one.
-      let draggedImage = ImageGallery.Image(imagePath: nil, aspectRatio: 1)
-      insertImage(draggedImage, at: destinationIndexPath)
-      
-      // Loads the image.
-      // TODO: Check if it's possible to add the placeholder only after figuring out what's the aspect ratio.
-      _ = item.dragItem.itemProvider.loadObject(ofClass: UIImage.self){ (provider, error) in
-        DispatchQueue.main.async {
-          if let image = provider as? UIImage {
-            self.gallery.images[destinationIndexPath.item].aspectRatio = image.aspectRatio
-            placeholderContext.setNeedsCellUpdate()
+        
+        // Creates a new image to hold the place for the dragged one.
+        let draggedImage = ImageGallery.Image(imagePath: nil, aspectRatio: 1)
+        insertImage(draggedImage, at: destinationIndexPath)
+        
+        // Loads the image.
+        // TODO: Check if it's possible to add the placeholder only after figuring out what's the aspect ratio.
+        _ = item.dragItem.itemProvider.loadObject(ofClass: UIImage.self){ (provider, error) in
+          DispatchQueue.main.async {
+            if let image = provider as? UIImage {
+              self.gallery.images[destinationIndexPath.item].aspectRatio = image.aspectRatio
+              placeholderContext.setNeedsCellUpdate()
+            }
           }
         }
-      }
-      
-      // TODO: So many nested blocks. Refactor this code.
-      // Loads the URL.
-      _ = item.dragItem.itemProvider.loadObject(ofClass: URL.self) { (provider, error) in
         
-        if let url = provider?.imageURL {
-          self.gallery.images[destinationIndexPath.item].imagePath = url
-          
-          // Downloads the image from the fetched url.
-          URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-              if let data = data {
-                placeholderContext.commitInsertion {
-                  self.gallery.images[$0.item].imageData = data
+        // Loads the URL.
+        _ = item.dragItem.itemProvider.loadObject(ofClass: URL.self) { (provider, error) in
+          if let url = provider?.imageURL {
+            self.gallery.images[destinationIndexPath.item].imagePath = url
+            
+            // Downloads the image from the fetched url.
+            URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+              DispatchQueue.main.async {
+                if let data = data {
+                  placeholderContext.commitInsertion {
+                    self.gallery.images[$0.item].imageData = data
+                  }
+                } else {
+                  // There was an error. Remove the placeholder.
+                  placeholderContext.deletePlaceholder()
                 }
-              } else {
-                // There was an error. Remove the placeholder.
-                placeholderContext.deletePlaceholder()
               }
-            }
-          }.resume()
-          
+            }.resume()
+          }
         }
         
       }
+
     }
   }
   
